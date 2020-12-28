4d5a964fc23480aaf0dbaf31ab68c3568e1ef479
Kotodian <417350372@qq.com>
2020-12-28 13:35:38 +0800

    register user cli

--- atomic_cli/cmd/server.go
+++ atomic_cli/cmd/server.go
@@ -36,7 +36,9 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		register.UserServiceRegister(port)
+		if serviceName == "user" {
+			register.UserServiceRegister(port)
+		}
 	},
 }
 
