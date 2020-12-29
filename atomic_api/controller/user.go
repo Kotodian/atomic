37bd58f6f7b73c25528394658de06a5451fbca77
Kotodian <417350372@qq.com>
2020-12-29 11:27:25 +0800

    完成用户更新api接口

--- atomic_api/controller/user.go
+++ atomic_api/controller/user.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"atomic/atomic_api/middleware"
 	pbUser "atomic/atomic_proto/user"
 	"atomic/internal/etcd"
 	"atomic/internal/service"
@@ -30,37 +31,55 @@ func WebUser(engine *gin.Engine, port int) {
 	cli := pbUser.NewUserService(service.InnerUser, micro.NewService(micro.Name(service.InnerUser), micro.Registry(reg)).Client())
 
 	routerUser := engine.Group("user")
-	{
-		routerUser.POST("/login", func(ctx *gin.Context) {
-			req := &pbUser.LoginRequest{}
-			err := ctx.ShouldBindJSON(req)
-			if err != nil {
-				ctx.JSON(http.StatusBadRequest, err)
-				return
-			}
-			response, err := cli.Login(ctx, req)
-			if err != nil {
-				ctx.JSON(http.StatusInternalServerError, err)
-				return
-			}
-			ctx.JSON(http.StatusOK, response)
-		})
+	// 用户登录api接口
+	routerUser.POST("/login", func(ctx *gin.Context) {
+		req := &pbUser.LoginRequest{}
+		err := ctx.ShouldBindJSON(req)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, err)
+			return
+		}
+		response, err := cli.Login(ctx, req)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, err)
+			return
+		}
+		ctx.JSON(http.StatusOK, response)
+	})
+	// 用户注册api接口
+	routerUser.POST("/register", func(ctx *gin.Context) {
+		req := &pbUser.RegisterRequest{}
+		err := ctx.ShouldBindJSON(req)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, err)
+			return
+		}
+		response, err := cli.Register(ctx, req)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, err)
+			return
+		}
+		ctx.JSON(http.StatusOK, response)
+	})
 
-		routerUser.POST("/register", func(ctx *gin.Context) {
-			req := &pbUser.RegisterRequest{}
-			err := ctx.ShouldBindJSON(req)
-			if err != nil {
-				ctx.JSON(http.StatusBadRequest, err)
-				return
-			}
-			response, err := cli.Register(ctx, req)
-			if err != nil {
-				ctx.JSON(http.StatusInternalServerError, err)
-				return
-			}
-			ctx.JSON(http.StatusOK, response)
-		})
-	}
+	routerUser.Use(middleware.JWTAuthMiddleware())
+
+	// 用户更新api接口
+	routerUser.POST("/update", func(ctx *gin.Context) {
+		req := &pbUser.UpdateRequest{}
+		err := ctx.ShouldBindJSON(req)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, err)
+			return
+		}
+		response, err := cli.Update(ctx, req)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, err)
+			return
+		}
+		ctx.JSON(http.StatusOK, response)
+	})
+
 	err := srv.Init()
 	if err != nil {
 		panic(err)
